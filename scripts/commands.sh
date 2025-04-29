46c026cd850a0b796459d45b90ec4fcc856a62c0
john-e-moore <jmoore87jr@gmail.com>
2025-04-29 15:32:44 -0400

    env template and scripts

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,3 +1,26 @@
+#!/usr/bin/env bash
+# scripts/commands.sh
+
+########################################
+# Environment
+########################################
+source .env # Before running scripts
+
+########################################
+# Cloudformation
+########################################
+aws cloudformation deploy \
+  --stack-name pipeline-roles \
+  --template-file roles.yaml \
+  --parameter-overrides \
+      Repo=$BUCKET_NAME/$S3_PREFIX \
+      BucketName=$BUCKET_NAME \
+      S3Prefix=$S3_PREFIX \
+  --capabilities CAPABILITY_NAMED_IAM
+
+########################################
+# SAM / Lambda
+########################################
 # Build
 sam build --use-container
 
@@ -5,16 +28,47 @@ sam build --use-container
 sam local invoke
 
 # Deploy
+## Stage
 sam deploy --config-env stage
-
-# Deploy & change samconfig
+### Deploy & change samconfig
 sam deploy --stack-name pipeline-stage \
-  --parameter-overrides Env=stage BucketName=tradesecretsdata S3Prefix=scraper-dk RoleArn=arn:aws:iam::688035104276:role/lambda-stage \
-  --config-env stage --guided
+  --parameter-overrides \
+    Env=stage \
+    BucketName=$BUCKET_NAME \
+    S3Prefix=$S3_PREFIX \
+    RoleArn=arn:aws:iam::688035104276:role/lambda-stage \
+  --config-env stage \
+  --guided
 
+## Prod
+sam deploy --config-env prod
+### Deploy & change samconfig
 sam deploy --stack-name pipeline-prod \
-  --parameter-overrides Env=prod BucketName=tradesecretsdata S3Prefix=scraper-dk RoleArn=arn:aws:iam::688035104276:role/lambda-prod \
-  --config-env prod --guided
+  --parameter-overrides \
+    Env=prod \
+    BucketName=$BUCKET_NAME \
+    S3Prefix=$S3_PREFIX \
+    RoleArn=arn:aws:iam::688035104276:role/lambda-prod \
+  --config-env prod \
+  --guided
 
-# Check out S3 bucket
-aws s3 ls s3://tradesecretsdata/scraper-dk/stage/raw/ | tail
+########################################
+# Make sure stuff is working
+########################################
+
+# S3
+## View latest raw data in s3 (stage)
+aws s3 ls s3://$BUCKET_NAME/$S3_PREFIX/stage/raw/ | tail
+
+# Duckdb
+## Download latest duckdb file (stage)
+aws s3 cp s3://$BUCKET_NAME/$S3_PREFIX/stage/db/$S3_PREFIX.duckdb .
+
+## Show the 5 most recent rows in a single shot
+duckdb $S3_PREFIX.duckdb -c \
+  "SELECT * FROM readings ORDER BY scraped_at DESC LIMIT 5;"
+
+## See min/max timestamps
+duckdb $S3_PREFIX.duckdb -c \
+  "SELECT MIN(scraped_at), MAX(scraped_at) FROM readings;"
+
