c1dbabdf87694433fe661e44c3e03ee93610014a
john-e-moore <jmoore87jr@gmail.com>
2025-04-29 15:00:31 -0400

    .

--- scripts/deploy_cloudformation.sh
+++
@@ -1,9 +0,0 @@
-aws cloudformation deploy \
-  --stack-name pipeline-roles \
-  --template-file roles.yaml \
-  --parameter-overrides \
-      Repo=tradesecretsdata/scraper-dk \
-      BucketName=tradesecretsdata \
-      S3Prefix=scraper-dk \
-  --capabilities CAPABILITY_NAMED_IAM
-
